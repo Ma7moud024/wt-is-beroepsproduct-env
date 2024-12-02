6e3382244956c892336aa6fcd900894d0043f780
Ma7moud024 <M.ZindAlHadid@student.han.nl>
2024-12-02 15:14:00 +0100

    start met php

---
+++ applicatie/test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> welkom wereld!</h1>
+    <?php
+    echo "halal";
+    ?>
+</body>
+</html>
