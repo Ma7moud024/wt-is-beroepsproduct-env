51cde70fc11fbf43d36ec9e12e22868c064bd0f0
Ma7moud024 <M.ZindAlHadid@student.han.nl>
2025-12-09 12:43:07 +0100

    .

---
+++ applicatie/les6/test1.php
@@ -0,0 +1,60 @@
+<?php
+ 
+$menu = [
+    "Eten" => ["Shoarma" => 6.95, "Doner" => 4.50],
+    "Drinken" => ["Cola" => 2.95, "Ayran" => 2.00],
+    "Kranten" => ["De Gelderlander" => 2.95, "Trouw" => 2.00]
+];
+ 
+$htmlMenu="";
+foreach ($menu as $categorie => $submenu) {
+    $htmlMenu .= "<h2>$categorie</h2>";
+    $htmlMenu .= "<table>";
+
+    foreach ($submenu as $item => $price) {
+        $htmlMenu .= "<tr><td>" . $item . "</td><td>&euro; " . $price . "</td></tr>";
+    }
+ 
+    
+ 
+    $htmlMenu .= "</table>";
+}
+?>
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Restaurantmenu</title>
+    <style>
+      td:first-child {
+        width: 8em;
+      }
+      td:nth-child(2) {
+        font-style: italic;
+        text-align: right;
+        width: 4em;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Menu</h1>
+ 
+    <?= $htmlMenu ?>
+ 
+    <h2>Eten</h2>
+    <table>
+        <tr><td>Shoarma</td><td>&euro; 6.95</td></tr>
+        <tr><td>Appels</td><td>&euro; 10.95</td></tr>
+        <tr><td>Tabouleh</td><td>&euro; 8.95</td></tr>
+        <tr><td>Hamburger</td><td>&euro; 5.50</td></tr>
+    </table>
+ 
+    <h2>Drinken</h2>
+    <table>
+        <tr><td>Cola</td><td>&euro; 2.00</td></tr>
+        <tr><td>Ayran</td><td>&euro; 2.30</td></tr>
+        <tr><td>Fernandes</td><td>&euro; 2.50</td></tr>
+        <tr><td>Bier</td><td>&euro; 5.50</td></tr>
+    </table>
+  </body>
+</html>
